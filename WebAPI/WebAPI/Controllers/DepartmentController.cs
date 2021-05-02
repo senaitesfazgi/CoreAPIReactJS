d0b21ab91288f8d680f3ef4fc1af314771e5c256
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-05-02 02:32:43 -0600

    Put method added to controller API

--- WebAPI/WebAPI/Controllers/DepartmentController.cs
+++ WebAPI/WebAPI/Controllers/DepartmentController.cs
@@ -75,5 +75,33 @@ namespace WebAPI.Controllers
             return new JsonResult("Added Successfully");
         }
 
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            string query = @"
+                    update dbo.Department set 
+                    DepartmentName = '" + dep.DepartmentName + @"'
+                    where DepartmentId = " + dep.DepartmentId + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+
     }
 }
