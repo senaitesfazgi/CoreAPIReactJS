ce1fefcc8761c23e440e0ee979e21ca41a907065
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-05-03 00:19:04 -0600

    Added all endpoint API's

--- WebAPI/WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/WebAPI/Controllers/EmployeeController.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using WebAPI.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WebAPI.Controllers
 {
