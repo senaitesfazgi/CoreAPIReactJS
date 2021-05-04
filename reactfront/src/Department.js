7c2202f1aa0e6a8f1a803609b3d5fd6231f6048e
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-05-03 22:03:38 -0600

    Added AddDepModal.js

--- reactfront/src/Department.js
+++ reactfront/src/Department.js
@@ -1,11 +1,15 @@
 import React,{Component} from 'react';
 import {Table} from 'react-bootstrap';
 
+import {Button,ButtonToolbar} from 'react-bootstrap';
+import {AddDepModal} from './AddDepModal';
+import {EditDepModal} from './EditDepModal';
+
 export class Department extends Component{
 
     constructor(props){
         super(props);
-        this.state={deps:[]}
+        this.state={deps:[], addModalShow:false}
     }
 
     refreshList(){
@@ -26,7 +30,7 @@ export class Department extends Component{
 
     render(){
         const {deps}=this.state;
-
+        let addModalClose=()=>this.setState({addModalShow:false});
         return(
             <div>
                 <Table className="mt-4" striped bordered hover size="sm">
@@ -46,6 +50,14 @@ export class Department extends Component{
                             </tr>)}
                     </tbody>
                 </Table>
+                <ButtonToolbar>
+                    <Button variant='primary'
+                    onClick={()=>this.setState({addModalShow:true})}>
+                    Add Department</Button>
+
+                    <AddDepModal show={this.state.addModalShow}
+                    onHide={addModalClose}/>
+                </ButtonToolbar>
 
             </div>
         )
